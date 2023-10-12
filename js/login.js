fdd77ff82de41f32d16c1a17f8bf200e61791e74
Goune <71518163+Goune1@users.noreply.github.com>
2023-10-12 21:48:20 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 function auth() {
     var email = document.getElementById("email").value
     var password = document.getElementById("password").value
-    if(email == "goune.ledreo@gmail.com" && password == "goune1407") {
+    if(email == "gounea.a@gmail.com" && password == "ratio") {
         window.location.assign("dashboard.html")
         alert("OK")
     } 
